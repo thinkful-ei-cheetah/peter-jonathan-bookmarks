af9dffe869ae9600b7a4e994b336faf738126fdd
elang5 <green.elan5@gmail.com>
2019-05-02 12:35:20 -0500

    add dynamic routing

--- src/app.js
+++ src/app.js
@@ -7,10 +7,22 @@ const { NODE_ENV } = require('./config');
 
 const app = express();
 
+var whitelist = ['http://localhost:8000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if(!origin) return callback(null, true)
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 
 app.get('/', (req, res) => {
